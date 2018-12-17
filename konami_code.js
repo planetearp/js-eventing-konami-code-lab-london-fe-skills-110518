5097910def9a27aa7b74c21c8eabf34dfa6eb499
Thiago de Sa Earp Pagliarelli <thiagoearp@gmail.com>
2018-12-17 19:00:57 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
+  function onKeyDownHandler(e) {
+    console.log(e);
+    const key = parseInt(e.detail || e.which);
+    console.log(key);
+    
+    
+    if ( key === code[index] ) {
+      index ++;
+      console.log(index);
+      if ( index >= 10 ) {
+        alert('Congrats! You just got 30 lives!');
+      } else {
+      console.log(index);
+    } 
+    }
+  }
+  
+  
+  document.body.addEventListener('keydown', onKeyDownHandler)
 
 }
+init();
